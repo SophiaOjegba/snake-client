7c1751f8816d9e49e0f46ecaf871e04051690f30
sophia ojegba <roseysofia@yahoo.com>
2023-07-15 18:19:15 -0400

    updated codes with comments

--- input.js
+++ input.js
@@ -1,7 +1,8 @@
-const { stdin } = require("process");
-
+// Declaring a variable to store the TCP connection
 let connection;
 
+
+// Defining the function to set up input handling
 const setupInput = function (conn) {
   connection = conn;
   const stdin = process.stdin;
@@ -13,27 +14,30 @@ const setupInput = function (conn) {
   return stdin;
 };
 
-
-
+// Defining the function to handle user input
 const handleUserInput = function (key) {
+  // If "Ctrl + C" is pressed, exit the process
   if (key === '\u0003') {
     process.exit();
-  }else if (key === 'n') {
-    //user chat message
-  connection.write("Say: Hello")
-}
-
-  
-  let alphabet = {
-    w : "Move: up",
-    a : "Move: left",
-    d : "Move: right",
-    s : "Move: down"
   }
-  if (key in alphabet)
-  connection.write(alphabet[key]);
+  // If "n" key is pressed, send a chat message "Say: Hello" to the server
+  else if (key === 'n') {
+    connection.write("Say: Hello");
+  }
 
+  // Object that maps keyboard inputs to movement commands
+  let alphabet = {
+    w: "Move: up",
+    a: "Move: left",
+    d: "Move: right",
+    s: "Move: down"
+  };
+
+  // If the key pressed is a valid movement key, send the corresponding movement command to the server
+  if (key in alphabet) {
+    connection.write(alphabet[key]);
+  }
 };
 
-
-module.exports = {setupInput}
+// Exporting the "setupInput" function 
+module.exports = { setupInput };
