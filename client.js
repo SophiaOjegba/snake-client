7afd7884125f8f02f8f10f68af5161bb7df6c0aa
sophia ojegba <roseysofia@yahoo.com>
2023-07-15 13:55:32 -0400

    Added setimeout to move the snake in the game server

--- client.js
+++ client.js
@@ -1,34 +1,65 @@
 const net = require("net");
 
-// establishes a connection with the game server
+// Establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: 'localhost', 
+    host: 'localhost',
     port: 50541
-    
   });
 
-  // interpret incoming data as text
+  // Interpret incoming data as text
   conn.setEncoding("utf8");
-  
+
+  let currentDirection = null; // snake current direction
+
   conn.on("data", (data) => {
-  console.log("Server says: ", data);
+    console.log("Server says:", data);
   });
 
-  //write when connected to server
+  // Write when connected to server
   conn.on("connect", () => {
-  console.log("Successfully connected to game server");
+    console.log("Successfully connection");
+
+    // Send the name to the server
+    conn.write("Name: Sam");
   });
 
-  //write back to server, sending Name
-  conn.on("connect", () => {
-  conn.write("Name: ABC");
+  // Update currentDirection variable when receiving movement data
+  conn.on("data", (data) => {
+    if (data.includes("Moved")) {
+      const movement = data.split(" ")[1];
+      currentDirection = movement;
+    }
   });
 
-  return conn;
+  return {
+    conn,
+    getCurrentDirection: () => currentDirection // Expose a method to get the current direction
+  };
 };
 
 console.log("Connecting ...");
-connect();
+const { conn, getCurrentDirection } = connect();
 
-module.exports = {connect};
+// Move the snake up after a delay of 3 seconds if not moving down
+setTimeout(() => {
+  if (getCurrentDirection() !== "down") {
+    conn.write("Move: up");
+  }
+}, 3000);
+setTimeout(() => {
+  if (getCurrentDirection() !== "left") {
+    conn.write("Move: right");
+  }
+}, 5000);
+setTimeout(() => {
+  if (getCurrentDirection() !== "right") {
+    conn.write("Move: left");
+  }
+}, 7000);setTimeout(() => {
+  if (getCurrentDirection() !== "up") {
+    conn.write("Move: down");
+  }
+}, 9000);
+
+module.exports = { connect };
